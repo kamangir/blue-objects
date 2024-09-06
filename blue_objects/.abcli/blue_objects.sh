3fb914f6835ad2632498b591578522efabc5865d
kamangir <arash@kamangir.net>
2024-09-05 20:39:29 -0700

    pipeline fix, hopefully - kamangir/bolt#746

--- blue_objects/.abcli/blue_objects.sh
+++ blue_objects/.abcli/blue_objects.sh
@@ -3,6 +3,11 @@
 function blue_objects() {
     local task=$(abcli_unpack_keyword $1 help)
 
+    if [ $task == "help" ]; then
+        blue_objects version
+        return
+    fi
+
     abcli_generic_task \
         plugin=blue_objects,task=$task \
         "${@:2}"
