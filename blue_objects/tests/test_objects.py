12b3afbdb7b1c42277523401c3b900582cb4dc79
kamangir <arash@kamangir.net>
2024-09-13 19:47:47 -0700

    test++ - kamangir/bolt#746

--- blue_objects/tests/test_objects.py
+++ blue_objects/tests/test_objects.py
@@ -12,29 +12,53 @@ from blue_objects.env import VANWATCH_TEST_OBJECT
 def test_objects_download(object_name):
     assert objects.download(object_name)
 
+
+@pytest.mark.parametrize(
+    ["object_name", "cloud"],
+    [
+        [VANWATCH_TEST_OBJECT, True],
+        [VANWATCH_TEST_OBJECT, False],
+    ],
+)
+def test_objects_list_of_files(
+    object_name: str,
+    cloud: bool,
+):
+    assert objects.download(object_name)
+
     list_of_files = [
         file.name_and_extension(filename)
-        for filename in objects.list_of_files(object_name=object_name)
+        for filename in objects.list_of_files(
+            object_name=object_name,
+            cloud=cloud,
+        )
     ]
 
-    assert "vancouver.json" in list_of_files, objects.path_of(
-        object_name=object_name,
-        filename="vancouver.json",
-    )
+    assert "vancouver.json" in list_of_files
 
-    assert file.exist(
+
+def test_object_object_path():
+    object_name = objects.unique_object()
+    object_path = objects.object_path(object_name, create=True)
+    assert object_path
+    assert path.exists(object_path)
+
+
+@pytest.mark.parametrize(
+    ["object_name"],
+    [
+        [VANWATCH_TEST_OBJECT],
+    ],
+)
+def test_objects_path_of(object_name):
+    assert objects.download(object_name)
+
+    assert file.exists(
         objects.path_of(
             object_name=object_name,
             filename="vancouver.json",
         )
     )
-
-
-def test_object_path():
-    object_name = objects.unique_object()
-    object_path = objects.object_path(object_name, create=True)
-    assert object_path
-    assert path.exists(object_path)
 
 
 def test_objects_unique_object():
