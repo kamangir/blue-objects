1fbd8e6a91d0fc74cd1edaf1cc841456a80bd878
kamangir <arash@kamangir.net>
2024-09-14 15:17:43 -0700

    test++ - kamangir/bolt#746

--- blue_objects/__init__.py
+++ blue_objects/__init__.py
@@ -4,7 +4,7 @@ ICON = "🌀"
 
 DESCRIPTION = f"{ICON} data objects for Bash."
 
-VERSION = "5.42.1"
+VERSION = "5.43.1"
 
 REPO_NAME = "blue-objects"
 
--- blue_objects/tests/test_file.py
+++
@@ -1,7 +0,0 @@
-from blue_objects import file
-
-# TODO: complete.
-
-
-def test_file():
-    assert True
