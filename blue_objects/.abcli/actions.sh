2b2a82ce7a09b647f215d1434c61d1fb88052c54
kamangir <arash@kamangir.net>
2024-10-13 13:09:43 -0700

    git refactor - kamangir/bolt#746

--- blue_objects/.abcli/actions.sh
+++ blue_objects/.abcli/actions.sh
@@ -2,7 +2,10 @@
 
 function blue_objects_action_git_before_push() {
     blue_objects build_README
+    [[ $? -ne 0 ]] && return 1
 
-    [[ "$(abcli_git get_branch)" == "main" ]] &&
-        blue_objects pypi build
+    [[ "$(abcli_git get_branch)" != "main" ]] &&
+        return 0
+
+    blue_objects pypi build
 }
