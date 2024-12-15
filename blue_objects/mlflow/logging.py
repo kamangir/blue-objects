7e649e6d7638df7ecec7791961871c89eb483a7d
kamangir <arash@kamangir.net>
2024-12-15 12:31:32 -0800

    start of mlflow-optimizations-2024-12-15-bfPqUe 🪄 - kamangir/bolt#746

--- blue_objects/mlflow/logging.py
+++ blue_objects/mlflow/logging.py
@@ -1,3 +1,4 @@
+from typing import Dict
 import os
 import glob
 import mlflow
@@ -50,19 +51,31 @@ def log_run(object_name: str) -> bool:
 
     object_path = objects.object_path(object_name, create=True)
 
+    counts: Dict[str, int] = {}
+    skipped_count = 0
     for extension in "dot,gif,jpeg,jpg,json,png,sh,xml,yaml".split(","):
         for filename in glob.glob(
             os.path.join(object_path, f"*.{extension}"),
         ):
+            filename_name = file.name(filename)
+
+            counts[len(filename_name)] = counts.get(len(filename_name), 0) + 1
+
             if any(
                 [
                     file.size(filename) > 10 * 1024 * 1024,
-                    file.name(filename).startswith("thumbnail"),
+                    filename_name.startswith("thumbnail"),
+                    counts[len(filename_name)] > 20,
                 ]
             ):
+                logger.info(f"skipping {filename}")
+                skipped_count += 1
                 continue
 
             mlflow.log_artifact(filename)
             logger.info(f"⬆️  {filename}")
+
+    if skipped_count:
+        logger.info(f"skipped {skipped_count:,} file(s).")
 
     return end_run(object_name)
