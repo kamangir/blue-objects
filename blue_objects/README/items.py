8a6a49bf8c82fcdf4ac2fe7a7afec87290604c9c
kamangir <arash@kamangir.net>
2025-02-19 17:37:38 -0800

    start of README-refactor-2025-02-19-i0v2rp 🪄 - kamangir/bolt#746

--- blue_objects/README/items.py
+++ blue_objects/README/items.py
@@ -6,21 +6,25 @@ def Items(
     items: List[Dict[str, str]],
 ) -> List[str]:
     return [
-        "[`{}`]({}) [![image]({})]({}) {}".format(
-            item["name"],
-            item.get(
-                "url",
-                "#",
-            ),
-            item.get(
-                "marquee",
-                "https://github.com/kamangir/assets/raw/main/blue-plugin/marquee.png?raw=true",
-            ),
-            item.get(
-                "url",
-                "#",
-            ),
-            item.get("description", ""),
+        (
+            "[`{}`]({}) [![image]({})]({}) {}".format(
+                item["name"],
+                item.get(
+                    "url",
+                    "#",
+                ),
+                item.get(
+                    "marquee",
+                    "https://github.com/kamangir/assets/raw/main/blue-plugin/marquee.png?raw=true",
+                ),
+                item.get(
+                    "url",
+                    "#",
+                ),
+                item.get("description", ""),
+            )
+            if "name" in item
+            else ""
         )
         for item in items
     ]
